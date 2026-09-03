cb0ba7dfb3789f64e2735869bc54355dae59008e
zumradeals <zumradeals@gmail.com>
2026-09-03 22:11:02 +0000

    feat: wire news and staff administration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\MatchController;
+use App\Http\Controllers\Admin\NewsPostController;
 use App\Http\Controllers\Admin\PlayerController;
+use App\Http\Controllers\Admin\StaffMemberController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -17,13 +19,14 @@ Route::view('/partenaires', 'partners')->name('partners');
 Route::view('/contact', 'contact')->name('contact');
 
 Route::middleware('guest')->group(function () {
-    Route::get('/admin/login', [LoginController::class, 'create'])->name('login');
-    Route::post('/admin/login', [LoginController::class, 'store'])->name('login.store');
+ Route::get('/admin/login',[LoginController::class,'create'])->name('login');
+ Route::post('/admin/login',[LoginController::class,'store'])->name('login.store');
 });
-
-Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', DashboardController::class)->name('dashboard');
-    Route::resource('players', PlayerController::class)->except('show');
-    Route::resource('matches', MatchController::class)->except('show')->parameters(['matches'=>'match']);
-    Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
+Route::middleware(['auth','admin'])->prefix('admin')->name('admin.')->group(function () {
+ Route::get('/',DashboardController::class)->name('dashboard');
+ Route::resource('players',PlayerController::class)->except('show');
+ Route::resource('matches',MatchController::class)->except('show')->parameters(['matches'=>'match']);
+ Route::resource('news',NewsPostController::class)->except('show')->parameters(['news'=>'news']);
+ Route::resource('staff',StaffMemberController::class)->except('show')->parameters(['staff'=>'staff']);
+ Route::post('/logout',[LoginController::class,'destroy'])->name('logout');
 });
